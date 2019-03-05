32ec82bdd1f390d7cc7f0809c9c9f6b5a8552e91
denny <plachenko@gmail.com>
2019-03-04 19:15:44 -0500

    added layout changes and vuedraggable component

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,23 @@
     </div>
     -->
     <FSControl />
-    <span style="position: absolute; top: 0px; left: 0px; z-index: 9999">
-      {{scale}}
-    </span>
+
+    <div class="menu left" ref="left_menu"></div>
+    <div class="menu right" ref="right_menu" id="layer_menu">
+      <div class="item_container">
+        <draggable
+          :list="layers"
+          :disabled="!enabled"
+          class="list-group"
+          @start="dragging = true"
+          @end="dragging = false" >
+            <div class="layer_item" v-for="(layer, idx) in layers" :key="idx">
+              <span v-if="layer.name">{{layer.name}}</span>
+            </div>
+            <button slot="header" @click="addLayer">Add</button>
+        </draggable>
+      </div>
+    </div>
     <div ref="can" :style="{transform: 'rotate('+rotation+'deg'+') scale('+scale+')'}" id="canvas-container">
     <!-- <div ref="can" :style="{transform: 'matrix('+scale+','+rotation+','+(-1*rotation)+','+scale+',0,0)'}" id="canvas-container"> -->
       <!--
@@ -20,17 +34,18 @@
       <div class="handle corner" style="left: -30px; bottom: -30px;"></div>
       <div class="handle corner" style="right: -30px; bottom: -30px;"></div>
       -->
-
     </div>
   </div>
 </template>
 
 <script>
 import FSControl from './components/FSControl'
+import draggable from 'vuedraggable'
 
 export default {
   name: 'app',
   components: {
+    draggable,
     FSControl: FSControl
   },
   mounted(){
@@ -77,12 +92,39 @@ export default {
       width: 0,
       scale: 1,
       temp_scale: 0,
-      rotation: 0
+      rotation: 0,
+      enabled: true,
+      layers: [
+        { name: "Background" },
+      ],
+      dragging: false
     }
   },
   methods: {
     resizeEvt () {
       this.centerCanvas()
+      this.sizeLeft()
+      this.sizeRight()
+    },
+    addLayer(){
+      this.layers.push({name: 'layer'})
+      this.sizeRight()
+    },
+    sizeRight(){
+      let margin = 30
+      let _left = this.$refs.left_menu
+      let _w = window.innerWidth - margin
+      let _h = window.innerHeight - margin
+      
+      _left.style.top = (_h / 2) - (_left.clientHeight / 2) + "px"
+    },
+    sizeLeft(){
+      let margin = 30
+      let _right = this.$refs.right_menu
+      let _w = window.innerWidth - margin
+      let _h = window.innerHeight - margin
+
+      _right.style.top = (_h / 2) - (_right.clientHeight / 2) + "px"
     },
     sizeCanvas(){
       let _can = this.canvas_container
@@ -96,6 +138,8 @@ export default {
       _can.style.width = _w + "px"
       _can.style.height = _h + "px"
 
+      this.sizeLeft()
+      this.sizeRight()
     },
     centerCanvas(){
       let _can = this.canvas_container
@@ -134,7 +178,7 @@ body{
   box-sizing: border-box; 
   float: left; 
   min-width:100px; 
-  border-radius: 0px 0px 0px 20px
+  border-radius: 0px 0px 0px 10px
 }
 
 #canvas-container {
@@ -143,6 +187,48 @@ body{
   left: 0px;
   background-color: #FFF;
   box-shadow: #444 2px 2px 3px
+}
+
+.menu{
+  position: absolute;
+  top: 0px;
+  background-color: #F00;
+  width: 150px;
+  z-index: 9999;
+  padding: 10px;
+  box-sizing: border-box;
+}
+.left {
+  border-radius: 0px 10px 10px 0px;
+  left: 0px;
+  height: 200px;
+  width: 60px;
+}
+
+.item{
+  background-color: #FFF;
+  padding: 10px;
+  margin: 10px 0px;
+}
+
+.right {
+  border-radius: 10px 0px 0px 10px;
+  right: 0px;
+  overflow: auto;
+}
+
+.right .item_container{
+  overflow: auto;
+  min-height: 200px;
+  max-height: 300px;
+}
+#layer_menu .layer_item {
+  min-height: 100px;
+  width: 100%;
+  background-color: #FFF;
+}
+#layer_menu .item:last-child {
+  border-bottom: none;
 }
 
 #app {
@@ -165,4 +251,5 @@ height: 100%; width: 20px;
   width: 100%; 
   height: 20px;
 }
+
 </style>
